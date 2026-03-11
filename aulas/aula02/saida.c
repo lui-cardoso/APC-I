b1606849ede2701cdc7eb1709392bb5699a77aea
unknown <2612130001@ACADEMICO.IESB.BR>
2026-03-11 10:57:55 -0300

    adiciona codigos da aula02

---
+++ aulas/aula02/saida.c
@@ -0,0 +1,17 @@
+/*
+textão
+*/
+#include <stdio.h> // inclusão de bibliotecas da linguagem
+
+int main () {
+    // 1. entrada
+
+    // 2. processamento
+
+    // 3. saída
+    printf("um texto qualquer\n");
+    printf("%i\n", 10);
+    printf("%f\n", 10.1234);
+
+    return 0;
+}
