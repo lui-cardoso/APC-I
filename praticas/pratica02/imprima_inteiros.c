11e7350e634375906aec69f18d70f7a43fe79ced
unknown <2612130001@ACADEMICO.IESB.BR>
2026-04-01 10:07:56 -0300

    feat: conclui pratica02. Fecha #3

---
+++ praticas/pratica02/imprima_inteiros.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+    int inteiro;
+    int negativo;
+    int zeros;
+
+    inteiro = 10;
+    negativo = -5;
+    zeros = 00004;
+
+    printf("%i %i %i", inteiro, negativo, zeros);
+
+    return 0;
+}
