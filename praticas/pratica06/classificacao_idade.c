eb0668223e71958ed6f2b34bdc1fb26de499abe0
Lui Cardoso <luiza6cardoso@gmail.com>
2026-05-13 09:07:21 -0300

    feat: conclui pratica06. Fecha #12

--- praticas/pratica06/classificacao_idade.c
+++ praticas/pratica06/classificacao_idade.c
@@ -13,13 +13,13 @@ int main() {
     */
 
     if (idade <= 12) {
-        printf("Você é criança.");
+        printf("Você é criança.\n");
     } else if (idade > 12 && idade <= 17) {
-        printf("Você é adolescente.");
+        printf("Você é adolescente.\n");
     } else if (idade > 17 && idade <= 64) {
-        printf("Você é adulto.");
+        printf("Você é adulto.\n");
     } else {
-        printf("Você é idoso.");
+        printf("Você é idoso.\n");
     }
 
     return 0;
